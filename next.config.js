5fc020b273b0625b683a895ee0027894c4cc9153
Valentino <valentinorodulfo1@gmail.com>
2026-09-10 09:16:28 -0300

    CSP: permitir imagenes de Supabase (partes diarios) y fuentes de Google

--- next.config.js
+++ next.config.js
@@ -22,9 +22,9 @@ const nextConfig = {
             value: [
               "default-src 'self'",
               `script-src ${scriptSrc}`,
-              "style-src 'self' 'unsafe-inline'",
-              "img-src 'self' data: blob:",
-              "font-src 'self' data:",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "img-src 'self' data: blob: https://*.supabase.co",
+              "font-src 'self' data: https://fonts.gstatic.com",
               "connect-src 'self' https://*.supabase.co wss://*.supabase.co",
               "object-src 'none'",
               "base-uri 'self'",
